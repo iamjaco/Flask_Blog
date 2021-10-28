f4ddb9f7cb345903255059b267d322843fb78414
iamjaco <iamjaco@4b5d82362e5e>
2021-10-28 13:53:08 +0000

    changets to header:#

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,10 +1,14 @@
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from passlib.hash import sha256_crypt
+from flask_misaka import Misaka
+
 
 from flask_app import app, db
 from flask_app.models import User, Post
 from flask_app.forms import PostForm
+
+Misaka(app)
 
 @app.route("/")
 def index():
